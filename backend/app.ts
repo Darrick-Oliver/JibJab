2944c276a7d23b255b3dfa88eb201ea467935db8
psandhu101 <prabdilsandhu707@gmail.com>
2022-11-13 23:15:57 -0800

    implemented post and get messages backend

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
-import { createExpressServer } from 'routing-controllers';
+import { createExpressServer, Action } from 'routing-controllers';
 import { bootstrapDB } from './bootstrap/db_init';
+import { userChecker } from './utils/userChecker';
 
 bootstrapDB();
 
@@ -8,4 +9,5 @@ export const app = createExpressServer({
     cors: true,
     controllers: [`${__dirname}/controllers/*.ts`], // we specify controllers we want to use
     routePrefix: '/api',
+    currentUserChecker: userChecker
 });
