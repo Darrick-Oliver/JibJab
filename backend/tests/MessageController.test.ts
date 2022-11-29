08cc259f9afeded42e23fb99ad4c11ffd1372719
Lucas Abdo <luc.abdo01@gmail.com>
2022-11-29 12:47:09 -0800

    changed tests to mongodb memory server

--- backend/tests/MessageController.test.ts
+++ backend/tests/MessageController.test.ts
@@ -1,29 +1,36 @@
 import { MessageController } from '../controllers/MessageController';
 import { bootstrapDB } from '../bootstrap/db_init';
-import mongoose from 'mongoose';
+const mongoose = require('mongoose')
 import { CurrentUser } from 'routing-controllers';
+import { MongoClient } from 'mongodb';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import 'dotenv/config';
 
 const mc = new MessageController();
 jest.setTimeout(10000);
 
+let mongod: MongoMemoryServer;
+let conn: MongoClient;
+
 beforeAll(async () => {
-    await mongoose.disconnect();
-    await bootstrapDB();
+    const mongod = await MongoMemoryServer.create();
+    const conn = await mongoose.connect(mongod.getUri(), {});
 });
 
 afterAll(async () => {
     await mongoose.disconnect();
-    console.log('MongDB disconnected');
+    if (mongod){
+        await mongod.stop();
+    }
 });
 
 test('Test - Send Message - fail', async () => {
     const email = 'asdf';
     const password = 'asdfasdfasdf';
-    
-    try{
-      const result = await mc.create('test', 5, 5, CurrentUser());
-    }
-    catch(err: any){
-      expect(err.error).toBe(true);
+
+    try {
+        const result = await mc.create('test', 5, 5, CurrentUser());
+    } catch (err: any) {
+        expect(err.error).toBe(true);
     }
 });
--- backend/tests/UserController.test.ts
+++ backend/tests/UserController.test.ts
@@ -1,45 +1,37 @@
 import { UserController } from '../controllers/UserController';
 import { bootstrapDB } from '../bootstrap/db_init';
-import mongoose from 'mongoose';
+const mongoose = require('mongoose')
+import { MongoClient } from 'mongodb';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import 'dotenv/config';
 
 const uc = new UserController();
 jest.setTimeout(10000);
 
+let mongod: MongoMemoryServer;
+let conn: MongoClient;
+
 beforeAll(async () => {
-    await mongoose.disconnect();
-    await bootstrapDB();
+    //await mongoose.disconnect();
+    //await bootstrapDB();
+
+    const mongod = await MongoMemoryServer.create();
+    const conn = await mongoose.connect(mongod.getUri(), {});
 });
 
 afterAll(async () => {
+    //await mongoose.disconnect();
+    //console.log('MongDB disconnected');
     await mongoose.disconnect();
-    console.log('MongDB disconnected');
-});
-
-test('userController - login', async () => {
-    const email = 'asdf';
-    const password = 'asdfasdfasdf';
-    const result = await uc.login(email, password);
-    expect(result.error).toBe(false);
-});
-
-test('userController - login fail', async () => {
-    const email = 'failedEmail';
-    const password = 'asdfasdfasdf';
-    try {
-        const result = await uc.login(email, password);
-    } catch (err: any) {
-        expect(err.error).toBe(true);
+    if (mongod){
+        await mongod.stop();
     }
 });
 
-test('userController - login fail - wrong pass', async () => {
+test('simple test', async () => {
     const email = 'asdf';
-    const password = 'wrongPass';
-    try {
-        const result = await uc.login(email, password);
-    } catch (err: any) {
-        expect(err.error).toBe(true);
-    }
+    const password = 'asdfasdfasdf';
+    expect(5).toBe(5);
 });
 
 //Register User Tests
@@ -64,52 +56,11 @@ test('userController - register - fail - null fields', async () => {
 });
 
 test('userController - register - fail - short password', async () => {
-  const username = '$trawberry';
-  const first_name = 'John';
-  const last_name = 'Travolta';
-  const email = 'helloFresh';
-  const password = 'less8';
-  try {
-      const result = await uc.register(
-          username,
-          first_name,
-          last_name,
-          email,
-          password
-      );
-  } catch (err: any) {
-      expect(err.error).toBe(true);
-  }
-});
-
-test('userController - register - fail - short username', async () => {
-  const username = 'jo';
-  const first_name = 'John';
-  const last_name = 'Travolta';
-  const email = 'helloFresh';
-  const password = 'thisPasswordWorks';
-  try {
-      const result = await uc.register(
-          username,
-          first_name,
-          last_name,
-          email,
-          password
-      );
-  } catch (err: any) {
-      expect(err.error).toBe(true);
-  }
-});
-
-
-//requires deletion of user in database everytime to pass
-/*
-test('userController - register - success', async () => {
-    const username = 'test';
-    const first_name = 'tester';
-    const last_name = 'testerson';
-    const email = 'test123';
-    const password = 'asdfasdf1234';
+    const username = '$trawberry';
+    const first_name = 'John';
+    const last_name = 'Travolta';
+    const email = 'helloFresh';
+    const password = 'less8';
     try {
         const result = await uc.register(
             username,
@@ -122,4 +73,65 @@ test('userController - register - success', async () => {
         expect(err.error).toBe(true);
     }
 });
-*/
+
+test('userController - register - fail - short username', async () => {
+    const username = 'jo';
+    const first_name = 'John';
+    const last_name = 'Travolta';
+    const email = 'helloFresh';
+    const password = 'thisPasswordWorks';
+    try {
+        const result = await uc.register(
+            username,
+            first_name,
+            last_name,
+            email,
+            password
+        );
+    } catch (err: any) {
+        expect(err.error).toBe(true);
+    }
+});
+
+test('userController - register - success', async () => {
+    const username = 'johnBoi';
+    const first_name = 'John';
+    const last_name = 'Travolta';
+    const email = 'helloFresh';
+    const password = 'thisPasswordWorks';
+    const result = await uc.register(
+            username,
+            first_name,
+            last_name,
+            email,
+            password);
+    expect(result.error).toBe(false);
+});
+
+test('userController - login fail', async () => {
+    const email = 'failedEmail';
+    const password = 'asdfasdfasdf';
+    try {
+        const result = await uc.login(email, password);
+    } catch (err: any) {
+        expect(err.error).toBe(true);
+    }
+});
+
+test('userController - login fail - wrong pass', async () => {
+    const email = 'helloFresh';
+    const password = 'wrongPass';
+    try {
+        const result = await uc.login(email, password);
+    } catch (err: any) {
+        expect(err.error).toBe(true);
+    }
+});
+
+test('userController - login', async () => {
+    const email = 'helloFresh';
+    const password = 'thisPasswordWorks';
+    const result = await uc.login(email, password);
+    expect(result.error).toBe(false);
+});
+
