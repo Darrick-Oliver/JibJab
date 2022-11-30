4b756456e972413fe1805da4c5b6c304e87a51ef
Lucas Abdo <luc.abdo01@gmail.com>
2022-11-29 22:10:20 -0800

    Finished user-controller and message-controller

--- backend/tests/UserController.test.ts
+++ backend/tests/UserController.test.ts
@@ -1,6 +1,6 @@
 import { UserController } from '../controllers/UserController';
 import { bootstrapDB } from '../bootstrap/db_init';
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 import { MongoClient } from 'mongodb';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import 'dotenv/config';
@@ -11,37 +11,29 @@ jest.setTimeout(10000);
 let mongod: MongoMemoryServer;
 let conn: MongoClient;
 
+//connect to database
 beforeAll(async () => {
-    //await mongoose.disconnect();
-    //await bootstrapDB();
-
     const mongod = await MongoMemoryServer.create();
     const conn = await mongoose.connect(mongod.getUri(), {});
 });
 
+//disconnect from database
 afterAll(async () => {
-    //await mongoose.disconnect();
-    //console.log('MongDB disconnected');
     await mongoose.disconnect();
-    if (mongod){
+    if (mongod) {
         await mongod.stop();
     }
 });
 
-test('simple test', async () => {
-    const email = 'asdf';
-    const password = 'asdfasdfasdf';
-    expect(5).toBe(5);
-});
-
 //Register User Tests
-
 test('userController - register - fail - null fields', async () => {
     const username = '';
     const first_name = '';
     const last_name = '';
     const email = '';
     const password = '';
+
+    //register user
     try {
         const result = await uc.register(
             username,
@@ -61,6 +53,8 @@ test('userController - register - fail - short password', async () => {
     const last_name = 'Travolta';
     const email = 'helloFresh';
     const password = 'less8';
+
+    //register user
     try {
         const result = await uc.register(
             username,
@@ -75,22 +69,24 @@ test('userController - register - fail - short password', async () => {
 });
 
 test('userController - register - fail - short username', async () => {
-  const username = 'jo';
-  const first_name = 'John';
-  const last_name = 'Travolta';
-  const email = 'helloFresh';
-  const password = 'thisPasswordWorks';
-  try {
-      const result = await uc.register(
-          username,
-          first_name,
-          last_name,
-          email,
-          password
-      );
-  } catch (err: any) {
-      expect(err.error).toBe(true);
-  }
+    const username = 'jo';
+    const first_name = 'John';
+    const last_name = 'Travolta';
+    const email = 'helloFresh';
+    const password = 'thisPasswordWorks';
+
+    //register user
+    try {
+        const result = await uc.register(
+            username,
+            first_name,
+            last_name,
+            email,
+            password
+        );
+    } catch (err: any) {
+        expect(err.error).toBe(true);
+    }
 });
 
 test('userController - register - success', async () => {
@@ -99,18 +95,23 @@ test('userController - register - success', async () => {
     const last_name = 'Travolta';
     const email = 'helloFresh';
     const password = 'thisPasswordWorks';
+
+    //register user
     const result = await uc.register(
-            username,
-            first_name,
-            last_name,
-            email,
-            password);
+        username,
+        first_name,
+        last_name,
+        email,
+        password
+    );
     expect(result.error).toBe(false);
 });
 
 test('userController - login fail', async () => {
     const email = 'failedEmail';
     const password = 'asdfasdfasdf';
+
+    //try login
     try {
         const result = await uc.login(email, password);
     } catch (err: any) {
@@ -121,6 +122,8 @@ test('userController - login fail', async () => {
 test('userController - login fail - wrong pass', async () => {
     const email = 'helloFresh';
     const password = 'wrongPass';
+
+    //try login
     try {
         const result = await uc.login(email, password);
     } catch (err: any) {
@@ -128,10 +131,11 @@ test('userController - login fail - wrong pass', async () => {
     }
 });
 
-test('userController - login', async () => {
+test('userController - login - success', async () => {
     const email = 'helloFresh';
     const password = 'thisPasswordWorks';
+
+    //try login
     const result = await uc.login(email, password);
     expect(result.error).toBe(false);
 });
-
